271c415646b87e535825efad75068017fd804769
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-09-18 23:13:02 +0800

    fix: add mp3 file meta info when do dify voice to text

--- voice/dify/dify_voice.py
+++ voice/dify/dify_voice.py
@@ -1,64 +1,65 @@
-
+import os
 import requests
 from bridge.reply import Reply, ReplyType
 from common.log import logger
 from config import conf
 from voice.voice import Voice
+from voice.audio_convert import any_to_mp3
 import datetime
 import random
 
 class DifyVoice(Voice):
-    def __init__(self):
-        self.api_key = conf().get("dify_api_key")
-        self.api_base = conf().get("dify_api_base") or "http://dify.hanfangyuan.cn/v1"
-
     def voiceToText(self, voice_file):
-        logger.debug("[Dify] voice file name={}".format(voice_file))
+        logger.debug("[DIFY VOICE] voice file name={}".format(voice_file))
         try:
+            # 测试发现dify不支持wav格式，支持mp3格式，统一转为mp3格式
+            filepath, ext = os.path.splitext(voice_file)
+            if ext != ".mp3":
+                old_voice_file = voice_file
+                voice_file = filepath + ".mp3"
+                if not os.path.exists(old_voice_file):
+                    any_to_mp3(old_voice_file, voice_file)
             files = {
-                'file': open(voice_file, 'rb')
-            }
-            data = {
-                'user': 'user_id'  # 请根据实际情况替换为正确的用户标识
+                'file': (os.path.basename(voice_file), open(voice_file, 'rb'), 'audio/mp3')
             }
             headers = {
-                'Authorization': 'Bearer ' + self.api_key
+                'Authorization': 'Bearer ' + conf().get("dify_api_key")
             }
             response = requests.post(
-                f'{self.api_base}/audio-to-text',
+                f'{conf().get("dify_api_base")}/audio-to-text',
                 headers=headers,
-                files=files,
-                data=data
+                files=files
             )
+            if response.status_code != 200:
+                logger.error(response.text)
+                response.raise_for_status()
             response_data = response.json()
             text = response_data['text']
             reply = Reply(ReplyType.TEXT, text)
-            logger.info("[Dify] voiceToText text={} voice file name={}".format(text, voice_file))
+            logger.info("[DIFY VOICE] voiceToText text={} voice file name={}".format(text, voice_file))
         except Exception as e:
-            logger.error(e)
+            logger.error("[DIFY VOICE] voiceToText error={}")
             reply = Reply(ReplyType.ERROR, "我暂时还无法听清您的语音，请稍后再试吧~")
         return reply
 
     def textToVoice(self, text):
-        logger.debug("[Dify] text={}".format(text))
+        logger.debug("[DIFY VOICE] text={}".format(text))
         try:
             data = {
-                'text': text,
-                'user': 'user_id'  # 请根据实际情况替换为正确的用户标识
+                'text': text
             }
             headers = {
-                'Authorization': 'Bearer ' + self.api_key,
-                'Content-Type': 'application/json'
+                'Authorization': 'Bearer ' + conf().get("dify_api_key")
             }
             response = requests.post(
-                f'{self.api_base}/text-to-audio',
+                f'{conf().get("dify_api_base")}/text-to-audio',
                 headers=headers,
                 json=data
             )
-            file_name = "tmp/" + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(0, 1000)) + ".wav"
+            file_name = "tmp/" + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(0, 1000)) + ".mp3"
             with open(file_name, 'wb') as f:
                 f.write(response.content)
-            logger.info("[Dify] textToVoice success, file_name={}".format(file_name))
+            logger.info("[DIFY VOICE] textToVoice success, file_name={}".format(file_name))
             reply = Reply(ReplyType.VOICE, file_name)
         except Exception as e:
             logger.error(e)
